39ac4bdf724181d6bc629ddb9a5c928d302b7fbb
Pranav Anand <pranavanand2410@gmail.com>
2020-08-15 16:52:58 +0530

    Add files via upload

---
+++ HackerRank Python/Find a string.py
@@ -0,0 +1,14 @@
+def count_substring(string, sub_string):
+    count = 0
+    for i in range(len(string)):
+        if string[i:].startswith(sub_string):
+            count += 1
+    return count
+
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+
+    count = count_substring(string, sub_string)
+    print(count)
