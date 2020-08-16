b04d97be77da2149a258e9ac4446065df573aa61
Pranav Anand <pranavanand2410@gmail.com>
2020-08-16 20:09:03 +0530

    Delete 158A Next Round.py

--- Codeforces Problemset/158A Next Round.py
+++
@@ -1,8 +0,0 @@
-n, k = map(int, input().split())
-count = 0
-
-numList = list(map(int, input().strip().split()))[:n]
-for num in numList:
-    if num >= numList[k - 1] and num > 0:
-        count += 1
-print(count)
