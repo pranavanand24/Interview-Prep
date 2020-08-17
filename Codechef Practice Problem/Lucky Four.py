554ea68327bca2971dc02110ee5fcfc685286617
Pranav Anand <pranavanand2410@gmail.com>
2020-08-17 20:43:14 +0530

    Add files via upload

---
+++ Codechef Practice Problem/Lucky Four.py
@@ -0,0 +1,10 @@
+try:
+    for j in range(int(input())):
+        count = 0
+        for i in input():
+            if (i == '4'):
+                count += 1
+        print(count)
+except:
+    pass
+
