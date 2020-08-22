8db31a1f9823351d14d3866f873353cbd4ccece5
Pranav Anand <pranavanand2410@gmail.com>
2020-08-22 20:37:45 +0530

    Add files via upload

---
+++ Codechef Practice Problem/Grade The Steel.py
@@ -0,0 +1,20 @@
+try:
+    for t in range(int(input())):
+        h,c,t = input().split()
+        h = int(h)
+        c = float(c)
+        t = int(t)
+        if h > 50 and c < 0.7 and t > 5600:
+            print(10)
+        elif h > 50 and c < 0.7 :
+            print(9)
+        elif  c < 0.7 and t > 5600 :
+            print(8)
+        elif  h > 50 and t > 5600 :
+            print(7)
+        elif h > 50 or c < 0.7 or t > 5600:
+            print(6)
+        else:
+            print(5)
+except:
+    pass
